5d4f33525fe201085d5340b69501007d60cfe567
Dusan Stamenkovic <dusan.stamenkovic@codeberas.io>
2023-05-03 17:16:00 +0200

    fixed footer scss

--- src/app/components/layout/footer/Footer.tsx
+++ src/app/components/layout/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./footer.module.scss";
+import styles from "./Footer.module.scss";
 import footerImg from "../../../../../public/assets/footer-imgs/footer.jpg";
 import Image from "next/image";
 
